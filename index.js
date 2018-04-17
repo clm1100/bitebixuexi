66335888610ade572a6fc076f940bedb426a8621
clm110 <275298711@qq.com>
2018-04-17 11:38:13 +0800

    添加Transaction类

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const Blockchain  = require("./BlockChain")
-const Block = require('./Block')
+const Blockchain  = require("./BlockChain.js");
+console.log(Blockchain)
+// const Block = require('./Block')
+// let savjeeCoin = new Blockchain();
+// savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
+// savjeeCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
 
+class AA {
+  fn(){
+    console.log("22222")
+  }
+}
+
+var aa = new AA();
+console.log(aa.fn)
